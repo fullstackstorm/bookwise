dd43cce5f1b0ae04b0fdbcf1e657475b3c0644b4
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-08-18 17:07:47 -0700

    Add new alembic versions.

---
+++ lib/db/migrations/versions/14b9adae3d1e_empty_migration.py
@@ -0,0 +1,25 @@
+"""Empty migration.
+
+Revision ID: 14b9adae3d1e
+Revises: 
+Create Date: 2023-08-18 15:23:03.430738
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '14b9adae3d1e'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
