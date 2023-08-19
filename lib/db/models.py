114cc40be17b709a5e21ea5a1b6a05061a4961db
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-08-18 22:29:09 -0700

    Migrate to simpler model for mvp.

--- lib/db/models.py
+++ lib/db/models.py
@@ -4,20 +4,6 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-author_genre_association = Table(
-    'author_genre_association',
-    Base.metadata,
-    Column('author_id', Integer, ForeignKey('authors.id')),
-    Column('genre_id', Integer, ForeignKey('genres.id'))
-)
-
-preferred_author_association = Table(
-    'preferred_authors',
-    Base.metadata,
-    Column('user_id', Integer, ForeignKey('users.id')),
-    Column('author_id', Integer, ForeignKey('authors.id'))
-)
-
 preferred_genre_association = Table(
     'preferred_genres',
     Base.metadata,
@@ -32,16 +18,6 @@ class Author(Base):
     name = Column(String())
 
     books = relationship('Book', backref='author')
-    genres = relationship(
-        'Genre', 
-        secondary=author_genre_association, 
-        back_populates='authors'
-    )
-    preferred_by_users = relationship(
-        'User', 
-        secondary=preferred_author_association, 
-        back_populates='preferred_authors'
-    )
 
 class Book(Base):
     __tablename__ = 'books'
@@ -57,11 +33,6 @@ class Genre(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String())
 
-    authors = relationship(
-        'Author', 
-        secondary=author_genre_association,
-        back_populates='genres'
-    )
     books = relationship('Book', backref='genres')
     preferred_by_users = relationship(
         'User',
@@ -75,11 +46,6 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String())
 
-    preferred_authors = relationship(
-        'Author', 
-        secondary=preferred_author_association, 
-        back_populates='preferred_by_users'
-    )
     preferred_genres = relationship(
         'Genre',
         secondary=preferred_genre_association,
