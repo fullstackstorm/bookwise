62532c7da19bf2a6de71270591afebbba409119c
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-08-19 11:28:46 -0700

    Add simple terminal menu package.

--- lib/cli.py
+++ lib/cli.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python
 from db.models.user import BookWiseUser
 
-current_user = BookWiseUser()
+class Cli():
+    def __init__(self):
+        current_user = None
 
-def start():
-    print("Welcome to BookWise! A CLI to give you new book recommendations.\n")
-    while True:
-        print("1. Login")
-        print("2. Exit\n")
-        user_input = input("Enter a selection (1-2):\n")
-        if user_input in ['1', '2']:
-            handle_user_input(user_input)
-            if user_input == '2':
-                break
-        else:
-            print("Invalid selection. Please choose 1 or 2.")
+    def start(self):
+        menu = True
+        print("Welcome to BookWise! A CLI to give you new book recommendations.\n")
+        while menu:
+            print("1. Login")
+            print("2. Exit\n")
+            user_input = input("Enter a selection (1-2):\n")
+            if user_input in ['1', '2']:
+                self.handle_user_input(user_input)
+                menu = False
+            else:
+                print("Invalid selection. Please choose 1 or 2.")
 
-def handle_user_input(choice):
-    if choice == '1':
-        current_user.login_or_create()
-    elif choice == '2':
-        print("Exiting the program.")
+    def second_menu():
+        pass
+
+    def handle_user_input(choice):
+        if choice == '1':
+            current_user = BookWiseUser.login_or_create()
+        elif choice == '2':
+            print("Exiting the program.")
 
 
-if __name__ == '__main__':
-    start()
+    if __name__ == '__main__':
+        start()
